5691ecede33bdcff22115983b1b909e2b26200b7
Andrew Strovers <ADStrovers@gmail.com>
2015-04-06 09:00:01 -0500

    fixes scope of projects.js function.

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,14 +1,14 @@
-function tagLoop() {
-  if (this.value === "all") {
-    window.location.href = href;
-  } else {
-    window.location.href = href +"?tags=" + this.value;
-  }
-};
-
 window.onload = function() {
   var tags = [];
   var href = window.location.origin + window.location.pathname;
+  
+  function tagLoop() {
+    if (this.value === "all") {
+      window.location.href = href;
+    } else {
+      window.location.href = href +"?tags=" + this.value;
+    }
+  };
   
   tags = document.getElementsByClassName("tag_radio");
   
